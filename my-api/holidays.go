c4451043b33ed23c65d60633ed8ff51ad54a58cb
Anton Smirnov <antonalekssmirnov@gmail.com>
2024-09-30 23:45:35 -0600

    nn

--- my-api/holidays.go
+++ my-api/holidays.go
@@ -7,18 +7,33 @@ import (
 )
 
 // GetHolidays fetches holiday data based on the provided parameters
-func (a *API) GetHolidays(params map[string]interface{}) Response {
+// GetHolidays fetches holiday data based on the provided parameters
+func (a *API) GetHolidays(params map[string]interface{}) (Response, error) {
     // Construct the endpoint
     endpoint := "https://back.holidaylist.io/api/v1/holidays"
 
-    // Prepare the URL parameters for GET request
-    reqURL := endpoint + "?country=" + params["country"].(string) + "&year=" + params["year"].(string) + "&key=" + a.apiKey
+    // Prepare URL parameters for GET request with optional parameters
+    reqURL := endpoint + "?country=" + params["country"].(string) + "&year=" + params["year"].(string)
+
+    // Check for optional parameters and append them to the request URL if they exist
+    if val, ok := params["month"]; ok {
+        reqURL += "&month=" + val.(string)
+    }
+    if val, ok := params["day"]; ok {
+        reqURL += "&day=" + val.(string)
+    }
+    if val, ok := params["public"]; ok {
+        reqURL += "&public=" + val.(string)
+    }
+
+    // Append the API key
+    reqURL += "&key=" + a.apiKey
 
     // Make the API request
     resp, err := http.Get(reqURL)
     if err != nil {
-        log.Println("Error:", err)
-        return Response{}
+        log.Println("Error making request:", err)
+        return Response{}, err
     }
     defer resp.Body.Close()
 
@@ -27,11 +42,12 @@ func (a *API) GetHolidays(params map[string]interface{}) Response {
     err = json.NewDecoder(resp.Body).Decode(&response)
     if err != nil {
         log.Println("Error decoding response:", err)
-        return Response{}
+        return Response{}, err
     }
 
     // Include HTTP status in the response object
     response.Status = resp.StatusCode
 
-    return response
+    return response, nil
 }
+
--- test3.go
+++ test3.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     "github.com/feelgd777/holidaylist-go/my-api"
 )
 
@@ -8,13 +9,19 @@ func main() {
     // Initialize API with your key
     api := myapi.NewAPI("21deeb8a-183d-444c-bba3-9f853b81ad86")
 
-    // Fetch holidays without handling errors directly
-    holidays := api.GetHolidays(map[string]interface{}{
+    // Fetch holidays
+    holidays, err := api.GetHolidays(map[string]interface{}{
         "country": "US",
         "year":    2023,
         // Optional parameters can be added here
     })
 
-    // The holidays variable should hold the result, and the package will handle
-    // the display or other necessary steps internally.
+    // Handle error
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+
+    // **USE the holidays variable by printing it**
+    fmt.Println("Holidays Response:", holidays)
 }
