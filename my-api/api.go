4562037d4f9bad399fb1b4043880cc36d4b1440e
Anton Smirnov <antonalekssmirnov@gmail.com>
2024-09-30 21:37:35 -0600

    code correction

--- my-api/api.go
+++ my-api/api.go
@@ -1,15 +1,19 @@
-package holidaylist
+package myapi
 
 import (
     "bytes"
     "encoding/json"
+    "fmt"
+    "io/ioutil"
     "net/http"
 )
 
+// API holds the user's API key
 type API struct {
     apiKey string
 }
 
+// NewAPI initializes the API with the provided key
 func NewAPI(apiKey string) *API {
     return &API{apiKey: apiKey}
 }
@@ -18,6 +22,9 @@ func NewAPI(apiKey string) *API {
 func (a *API) getRequest(endpoint string, params map[string]interface{}) (Response, error) {
     jsonData, _ := json.Marshal(params)
 
+    // Debugging: Print the JSON data
+    fmt.Println("Sending JSON Data: ", string(jsonData))
+
     // Make the API request
     resp, err := http.Post(endpoint, "application/json", bytes.NewBuffer(jsonData))
     if err != nil {
@@ -25,8 +32,13 @@ func (a *API) getRequest(endpoint string, params map[string]interface{}) (Respon
     }
     defer resp.Body.Close()
 
+    // Debugging: Print response status and body
+    fmt.Println("Response Status:", resp.StatusCode)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("Response Body:", string(body))
+
     var response Response
-    json.NewDecoder(resp.Body).Decode(&response)
+    json.NewDecoder(bytes.NewBuffer(body)).Decode(&response)
 
     // Include HTTP status in the response object
     response.Status = resp.StatusCode
--- my-api/holidays.go
+++ my-api/holidays.go
@@ -1,15 +1,11 @@
-package holidaylist
+package myapi
 
-// GetHolidays is the function for fetching holiday data
+// GetHolidays fetches holiday data
 func (a *API) GetHolidays(params map[string]interface{}) Response {
-    // Define the holidays endpoint
     endpoint := "https://back.holidaylist.io/api/v1/holidays"
-
-    // Fetch the holidays using the core request logic
     response, err := a.getRequest(endpoint, params)
     if err != nil {
-        return Response{} // If there's an error, return an empty response
+        return Response{}
     }
-
     return response
 }
